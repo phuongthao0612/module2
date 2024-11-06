eb86062d790c02a8f4893111128d19d0b70f365e
phuongthao0612 <phuong6thao@gmail.com>
2024-11-06 09:03:41 +0700

    fixloi

--- 1_java/Currency.java
+++ 1_java/Currency.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
 public class Currency {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double usd;
         Scanner sc = new Scanner(System.in);
         System.out.print("Moi ban nhap Usd:");
         usd = sc.nextDouble();
-        double convert = usd * 23000;
-        System.out.print("Vnd = "+ convert);
+        if (usd > 0) {
+            double convert = usd * 23000;
+            System.out.print("Vnd = " + convert);
+        } else {
+            System.out.print("Nhap Usd lon hon 0");
+        }
     }
 }
